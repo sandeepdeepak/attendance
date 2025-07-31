347ad3541107a09e1fe630f145aa9d4eb9dd96fc
sandeepdeepak <44576019+sandeepdeepak@users.noreply.github.com>
2025-07-31 15:34:08 +0530

    fixed vite.config for PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Add Tailwind
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "SD GYM",
+        short_name: "SD GYM",
+        description:
+          "Managing gym members and attendance and thier diet plans and workout plans",
+        theme_color: "#1E40AF", // Tailwind blue-800 for example
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+      },
+      devOptions: {
+        enabled: true, // Enable SW in dev for easier testing
+      },
+    }),
   ],
 });
