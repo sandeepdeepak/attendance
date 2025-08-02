a7093eb08dba10a9b276d4486536aa6a1e71c0e1
sandeepdeepak <44576019+sandeepdeepak@users.noreply.github.com>
2025-08-03 00:36:23 +0530

    Fixed icon url in service-worker

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,13 +1,18 @@
 self.addEventListener("push", (event) => {
   console.log("Push event received:", event);
+  console.log("self.registration", self.registration);
   const data = event.data?.json() || {};
 
   const title = data.title || "Notification";
   const options = {
     body: data.body || "You have a new message.",
-    icon: "./assets/pwa-192x192.png",
-    badge: "./assets/pwa-192x192.png",
+    icon: "pwa-192x192.png",
+    badge: "pwa-192x192.png",
   };
+
+  console.log("Notification data:", data);
+  console.log("Notification title:", title);
+  console.log("Notification options:", options);
 
   event.waitUntil(self.registration.showNotification(title, options));
 });
